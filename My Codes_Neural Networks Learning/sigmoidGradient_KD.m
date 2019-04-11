24f450d83bd9047381c8676588c30b2b9ae4ae85
KateD <42650350+k8dix@users.noreply.github.com>
2019-04-11 10:49:25 -0400

    Add files via upload

---
+++ My Codes_Neural Networks Learning/sigmoidGradient_KD.m
@@ -0,0 +1,29 @@
+function g = sigmoidGradient_KD(z)
+%SIGMOIDGRADIENT returns the gradient of the sigmoid function
+%evaluated at z
+%   g = SIGMOIDGRADIENT(z) computes the gradient of the sigmoid function
+%   evaluated at z. This should work regardless if z is a matrix or a
+%   vector. In particular, if z is a vector or matrix, you should return
+%   the gradient for each element.
+
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the gradient of the sigmoid function evaluated at
+%               each value of z (z can be a matrix, vector or scalar).
+
+
+gofz = 1.0 ./ (1.0 + exp(-z));
+two = 1 - gofz;
+g = gofz .* two;
+
+
+
+
+
+% =============================================================
+
+
+
+
+end
